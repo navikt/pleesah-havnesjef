fa9e82d7397b273c7f4623bbe3185f26b1d72b87
Kyrre Havik <kyrre.havik@nav.no>
2026-06-30 11:49:32 +0200

    🔥 trenger ikke indexhandler

---
+++ internal/api/team_handler.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Example: POST /api/v1/team/?team=team-a
+func (a *api) TeamHandler(w http.ResponseWriter, r *http.Request) {
+	team := r.URL.Query().Get("team")
+	if team == "" {
+		a.log.Error("missing team query parameter")
+		http.Error(w, "missing team query parameter", http.StatusBadRequest)
+		return
+	}
+
+	k8sconfig, err := a.k8s.SetupTeam(r.Context(), team)
+	if err != nil {
+		a.log.Error("failed creating team", "error", err, "team", team)
+		http.Error(w, "missing team query parameter", http.StatusBadRequest)
+		return
+	}
+
+	a.log.Info("Created new team", "team", team)
+
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(map[string]any{
+		"kubeconfig": k8sconfig,
+	})
+}
