0c840f66dbf58f446ecc9afa093c6074123189b0
Kyrre Havik <kyrre.havik@nav.no>
2026-06-30 12:31:06 +0200

    🐛 manglet timeout på http server

--- internal/api/api.go
+++ internal/api/api.go
@@ -3,9 +3,16 @@ package api
 import (
 	"log/slog"
 	"net/http"
+	"time"
 
 	"github.com/navikt/pleesah-havnesjef/internal/k8s"
 )
+
+type api struct {
+	k8s    k8s.Client
+	log    *slog.Logger
+	server *http.Server
+}
 
 func New(client k8s.Client, log *slog.Logger) api {
 	a := api{
@@ -13,20 +20,25 @@ func New(client k8s.Client, log *slog.Logger) api {
 		log: log,
 	}
 
-	http.HandleFunc("GET /api/v1/service/status", a.ServiceRunningHandler)
-	http.HandleFunc("POST /api/v1/team/", a.TeamHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /api/v1/service/status", a.ServiceRunningHandler)
+	mux.HandleFunc("POST /api/v1/team/", a.TeamHandler)
 
+	server := &http.Server{
+		Addr:           ":8080",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	a.server = server
 	return a
 }
 
 func (a api) Run() {
 	a.log.Info("Running on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := a.server.ListenAndServe(); err != nil {
 		panic(err.Error())
 	}
-}
-
-type api struct {
-	k8s k8s.Client
-	log *slog.Logger
 }
