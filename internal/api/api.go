15d83fa4fb0bf8a0d63d541941c038b4daba34c3
Kyrre Havik <kyrre.havik@nav.no>
2026-07-02 09:46:23 +0200

    ♻️ litt bedre flyt i APIet

--- internal/api/api.go
+++ internal/api/api.go
@@ -22,7 +22,7 @@ func New(client k8s.Client, log *slog.Logger) api {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /api/v1/status/", a.StatusHandler)
-	mux.HandleFunc("POST /api/v1/team/", a.TeamHandler)
+	mux.Handle("/api/v1/team/", http.StripPrefix("/api/v1/team", a.TeamHandler()))
 	mux.HandleFunc("GET /api/v1/teams", a.TreasureMapHandler)
 
 	server := &http.Server{
--- internal/api/team_handler.go
+++ internal/api/team_handler.go
@@ -4,28 +4,29 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
-// Example: POST /api/v1/team/?team=team-a
-func (a *api) TeamHandler(w http.ResponseWriter, r *http.Request) {
-	team := r.URL.Query().Get("team")
-	if team == "" {
-		a.log.Error("missing team query parameter")
-		http.Error(w, "missing team query parameter", http.StatusBadRequest)
-		return
-	}
+func (a *api) TeamHandler() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /{team}/create", a.teamCreate)
 
+	return mux
+}
+
+// Example: POST /api/v1/team/{team}/create?hex={code}
+func (a *api) teamCreate(w http.ResponseWriter, r *http.Request) {
+	team := r.PathValue("team")
 	k8sconfig, err := a.k8s.SetupTeam(r.Context(), team)
 	if err != nil {
 		a.log.Error("failed creating team", "error", err, "team", team)
-		http.Error(w, "failed creating team", http.StatusBadRequest)
+		http.Error(w, "failed creating team", http.StatusInternalServerError)
 		return
 	}
 
 	a.log.Info("Created new team", "team", team)
 
 	w.Header().Set("Content-Type", "application/json")
-
 	buffer := new(bytes.Buffer)
 	err = json.Compact(buffer, []byte(k8sconfig))
 	w.Write(buffer.Bytes())
